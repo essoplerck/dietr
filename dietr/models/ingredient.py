f4ba9a6bef9d535307155dad3d03f0a61551cd73
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2017-12-01 14:23:54 +0100

    Add ingredient model

---
+++ dietr/models/ingredient.py
@@ -0,0 +1,7 @@
+class IngredientModel:
+    '''
+    The ingredient model. This class is used to communicate with the database to
+    extract end update data using the CRUD principle. Data may be exchaned using
+    a intermediate object.
+    '''
+    pass
