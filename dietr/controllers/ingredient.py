61a4fe5be6a8712887ff5733b0dad4b466b9176a
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2017-12-01 14:18:22 +0100

    Create ingredient controller

---
+++ dietr/controllers/ingredient.py
@@ -0,0 +1,12 @@
+class IngredientController:
+    def add(self):
+        pass
+
+    def delete(self, id):
+        pass
+
+    def edit(self, id):
+        pass
+
+    def view(self, id):
+        pass
