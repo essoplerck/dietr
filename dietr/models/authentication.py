a1fc39f12e46d21ed3a75b847c69fbff13432e50
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-05 16:38:09 +0100

    Add example methods to model

--- dietr/models/authentication.py
+++ dietr/models/authentication.py
@@ -2,4 +2,20 @@ class AuthenticationModel:
     '''
     The authentication model.
     '''
-    pass
+    def add_user(user):
+        pass
+
+    def get_user(username):
+        user = {
+            'username': username,
+            'hash': 0x00,
+            'salt': 0x00,
+            'email': 'example@domain.com',
+            'name': {
+                'first_name': 'John',
+                'middle_name': None,
+                'last_name': 'Doe'
+            }
+        }
+
+        return user
