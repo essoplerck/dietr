62ad18d0c32968d1e6dfa4920f95069d90366c3d
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-05 16:36:29 +0100

    Add authentication controller

---
+++ dietr/controllers/authentication.py
@@ -0,0 +1,11 @@
+from flask import render_template, request
+
+class AuthenticationController:
+    def join(self):
+        pass
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
