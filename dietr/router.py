d55cc876a155ecc71245d30d17f8d346f0a7940a
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-05 16:39:29 +0100

    Add routres for authentication

--- dietr/router.py
+++ dietr/router.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 
 from dietr.controllers.ingredient import IngredientController
+from dietr.controllers.authentication import AuthenticationController
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,3 +22,24 @@ class Router:
         action      = controller.view
 
         return action(id)
+
+    @app.route('/login')
+    def login():
+        controller  = AuthenticationController()
+        action      = controller.login
+
+        return action()
+
+    @app.route('/logout', methods = ['GET', 'POST'])
+    def logout():
+        controller  = AuthenticationController()
+        action      = controller.logout
+
+        return action()
+
+    @app.route('/join')
+    def join():
+        controller  = AuthenticationController()
+        action      = controller.join
+
+        return action()
