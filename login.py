f74f817e3d47c43bb0ed89800ca56fcd456be3c8
rensieeee <31250247+rensieeee@users.noreply.github.com>
2017-12-14 14:28:38 +0100

    Login
    Bugfix and added basic password forget system

--- login.py
+++ login.py
@@ -14,9 +14,9 @@ mysql.init_app(app)
 def login():
     html=""
     if request.method=="GET":
-        html+="<form id='loginForm' method='POST' action='/login'><table><tr><td>E-mailaddress: </td><td><input name='email' type='text' value='' /></td></tr><tr><td>Password: </td><td><input name='pwd' type='password' value='' /></td></tr><tr><td><input name='log' type='submit' value='Log in' /></td><td><input name='pwdf' type='submit' value='Wachtwoord vergeten?'</tr></table></form>"
+        html+="<form id='loginForm' method='POST' action='/login'><table><tr><td>E-mailaddress: </td><td><input name='email' type='text' value='' /></td></tr><tr><td>Password: </td><td><input name='pwd' type='password' value='' /></td></tr><tr><td><input name='log' type='submit' value='Log in' /></td><td><input name='log' type='submit' value='Wachtwoord vergeten?'</tr></table></form>"
     else:
-        if request.form["submit"]=="Log in":
+        if request.form["log"]=="Log in":
             email=request.form["email"]
             pwd=request.form["pwd"]
             conn=mysql.connect()
@@ -28,7 +28,19 @@ def login():
                 html+="Login succesvol! Welkom, "+userdata[1]+"."
             else:
                 html+="Login gefaald."
-        elif request.form["submit"]=="Wachtwoord vergeten?":
-            html+="Vul je e-mailadres en gebruikersnaam in:<br><form id='pwdForm' method='POST' action='/login'><table><tr><td>E-mailaddress: </td><td><input name='email' type='text' value='' /></td></tr><tr><td>Username: </td><td><input name='uname' type='text' value='' /></td></tr><tr><td><input name='showPwd' type='submit' value='Geef wachtwoord' /></td></tr></table></form>"
-
+        elif request.form["log"]=="Wachtwoord vergeten?":
+            html+="Vul je e-mailadres en gebruikersnaam in:<br><form id='pwdForm' method='POST' action='/login'><table><tr><td>E-mailaddress: </td><td><input name='email' type='text' value='' /></td></tr><tr><td>Username: </td><td><input name='uname' type='text' value='' /></td></tr><tr><td><input name='log' type='submit' value='Geef wachtwoord' /></td></tr></table></form>"
+        elif request.form["log"]=="Geef wachtwoord":
+            email=request.form["email"]
+            uname=request.form["uname"]
+            conn=mysql.connect()
+            cursor=conn.cursor()
+            query="select * from account where email=%s and username=%s"
+            data=(email, uname)
+            if cursor.execute(query, data)>0:
+                userdata=cursor.fetchone()
+                html+="Het wachtwoord van "+uname+" is: "+userdata[3]+"<br>"
+            else:
+                html+="Sorry, deze gebruiker is niet gevonden!<br>"
+            html+="<form id='goBack' method='GET' action='/login'><input name='log' type='submit' value='Ga terug' /></form>"
     return html
