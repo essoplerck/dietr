ac09dc78d498b327edc50759838df1dc0e948bc0
rensieeee <31250247+rensieeee@users.noreply.github.com>
2017-12-14 18:55:09 +0100

    Initial commit
    Making the layout for the account controller

---
+++ accountcontroller.py
@@ -0,0 +1,19 @@
+from flask import render_template
+from . import AccountModel
+
+class AccountController:
+    def add_account():
+        #Registreer
+        pass
+
+    def login():
+        #Login
+        pass
+
+    def view_account():
+        #View
+        pass
+
+    def change_account():
+        #Change
+        pass
