34de52a3b7e99794f8a5e32095b404f84930a02e
rensieeee <31250247+rensieeee@users.noreply.github.com>
2017-12-14 15:50:31 +0100

    Bugfix and MVC
    Made the userpage MVC compatible, and fixed bugs

--- userpage.py
+++ userpage.py
@@ -10,7 +10,7 @@ app.config["MYSQL_DATABASE_DB"]="renswnc266_dietr"
 app.config["MYSQL_DATABASE_HOST"]="185.182.57.56"
 mysql.init_app(app)
 
-def pwdToHash(pw):
+def pwd_to_hash(pw):
     return pw
 
 
@@ -31,10 +31,10 @@ def user(ide):
             email=data[4]
             html+="<strong>Hallo, "+name+"</strong><br>"
             html+="<form id='dataForm' method='POST' action='/user/"+ide+"'><table><tr><td>Username: </td><td><input name='uname' 'type='text' value='"+user+"' /></td></tr><tr><td>Current Password: </td><td><input name='curpwd' type='password' value='' /></td></tr><tr><td>New Password: </td><td><input name='newpwd' type='password' value='' /></td></tr><tr><td>Repeat new Password: </td><td><input name='rnewpwd' type='password' value='' /></td></tr><tr><td>E-mail address: </td><td><input name='email' type='text' value='"+email+"' /></td></tr><tr><td><input name='upTable' type='submit' value='Verander data' /></td></tr></table></form>"
-            #render_template(user.html, account=data)
+            #html=render_template(user.html, account=data)
         else:
             html+="Sorry, deze gebruiker is niet gevonden!"
-            #render_template(404.html)
+            #html=render_template(404.html)
     else:
         html=""
         user=""
@@ -48,31 +48,30 @@ def user(ide):
             user=data[2]
             pwd=data[3]
             email=data[4]
-            if pwd==pwdToHash(request.form["curpwd"]):
+            if pwd==pwd_to_hash(request.form["curpwd"]):
                 if len(request.form["newpwd"])>0:
                     if request.form["newpwd"]==request.form["rnewpwd"]:
-                        data=(request.form["uname"], pwdToHash(request.form["newpwd"]), request.form["email"], ide)
-                        acc.update_account(data[0], data[1], data[2], data[3])
+                        data[2]=request.form["uname"]
+                        data[3]=pwd_to_hash(request.form["newpwd"])
+                        data[4]=request.form["email"]
+                        acc.update_account(data[2], data[3], data[4], ide)
                         html+="Gegevens succesvol aangepast!<br>"
-                        #render_template(user, account=data)
+
                     else:
                         html+="Zorg ervoor dat het nieuwe wacthwoord twee keer correct is ingevuld!<br>"
-                        #render_template(user, account=data)
+
                 else:
-                    query="update account set username=%s, email=%s where id=%s;"
-                    data= (request.form["uname"], request.form["email"], ide)
-                    conn=mysql.connect()
-                    cursor=conn.cursor()
-                    cursor.execute(query, data)
-                    conn.commit()
-                    conn.close()
-                    cursor.close()
+                    data[2]=request.form["uname"]
+                    data[4]=request.form["email"]
+                    acc.update_account(data[2], data[4], ide)
                     html+="Gegevens succesvol aangepast!<br>"
             else:
                 html+="Het wachtwoord is niet correct ingevuld!<br>"
+            #html=render_template(user.html, account=data)
             html+="<strong>Hallo, "+name+"</strong><br>"
             html+="<form id='dataForm' method='POST' action='/user/"+ide+"'><table><tr><td>Username: </td><td><input name='uname' 'type='text' value='"+request.form["uname"]+"' /></td></tr><tr><td>Current Password: </td><td><input name='curpwd' type='password' value='' /></td></tr><tr><td>New Password: </td><td><input name='newpwd' type='password' value='' /></td></tr><tr><td>Repeat new Password: </td><td><input name='rnewpwd' type='password' value='' /></td></tr><tr><td>E-mail address: </td><td><input name='email' type='text' value='"+request.form["email"]+"' /></td></tr><tr><td><input name='upTable' type='submit' value='Verander data' /></td></tr></table></form>"
         else:
             html+="Sorry, deze gebruiker is niet gevonden!"
+            #html=render_template(404.html)
 
     return html
