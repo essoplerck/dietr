c423d76931c3a6f450ff730274b4242bc7535840
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-14 21:50:43 +0100

    Add model and controller

---
+++ dietr/controllers/pantry.py
@@ -0,0 +1,20 @@
+from ..models.pantry import PantryModel
+
+class PantryController:
+    def __init__(self):
+        self.model = PantryModel()
+
+    def add(self):
+        pass
+
+    def edit(self, id):
+        pass
+
+    def delete(self, id):
+        pass
+
+    def view(self, id):
+        pass
+
+    def overview(self):
+        pass
---
+++ dietr/models/pantry.py
@@ -0,0 +1,15 @@
+class PantryModel:
+    def add_ingredient(self, ingredient):
+        pass
+
+    def edit_ingredient(self, ingredient):
+        pass
+
+    def delete_ingredient(self, id):
+        pass
+
+    def get_ingredient(self, id):
+        pass
+
+    def get_pantry_ingredients(self, id):
+        pass
