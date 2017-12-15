6a028bceb643e80e25240f81901a3b38cf41639a
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2017-12-15 18:16:40 +0100

    Remove placeholder

--- dietr/models/session.py
+++ dietr/models/session.py
@@ -15,16 +15,4 @@ class SessionModel:
         pass
 
     def get_user(username):
-        user = {
-            'username': username,
-            'hash': 0x00,
-            'salt': 0x00,
-            'email': 'example@domain.com',
-            'name': {
-                'first_name': 'John',
-                'middle_name': None,
-                'last_name': 'Doe'
-            }
-        }
-
-        return user
+        pass
