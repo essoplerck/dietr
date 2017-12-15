889daa9e009dc04ff94572d982e661f237904c2b
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2017-12-15 14:41:33 +0100

    Stop intercepting routes

--- dietr/router.py
+++ dietr/router.py
@@ -6,11 +6,6 @@ class Router:
     def __init__(self):
         self.routes = {}
 
-        # Intercept all requests
-        @app.route('/')
-        @app.route('/<path:path>')
-        def resolve_route(path = ''):
-            return self.resolve_route(path)
 
     def register_route(self, url, action, methods = ['GET', 'POST']):
         # Compile regular expression
