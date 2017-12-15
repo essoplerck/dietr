01b88bb9ae59704160eb6cc7c8815c1d33bd64fa
MNDriuch <32654213+MNDriuch@users.noreply.github.com>
2017-12-15 16:39:50 +0100

    Update crawler_ah.py

--- crawler/crawler_ah.py
+++ crawler/crawler_ah.py
@@ -10,3 +10,5 @@ def allergiespider(url):
         for x in p.findAll(text=re.compile('Bevat:')):
             print(x)
 allergiespider('http://localhost:8050/render.html?url={}&timeout=30&wait=10'.format('http://www.ah.nl/producten/product/wi193444/calve-saus-fles-knoflook'))
+
+start splash: sudo docker run -p 8050:8050 -p 5023:5023 scrapinghub/splash
