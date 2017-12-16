72c276cf4df0d9fff42cf3469f9c64a5f4d7f3a3
rensieeee <31250247+rensieeee@users.noreply.github.com>
2017-12-16 16:25:00 +0100

    Error checking
    Added Good error checking, looking for valse values of data.

--- accountcontroller.py
+++ accountcontroller.py
@@ -6,17 +6,41 @@ class AccountController:
     acc=""
     def __init__(self):
         acc=AccountModel()
+        pass
 
     def add_account(name, username, password, confirm_password, email):
-        if password==confirm_password and len(password)>8:
+        data=[0, 0, 0, 0, 0]
+        if len(name.split(" "))==0:
+            data[0]=1
+            data[1]=1
+        elif name[0]==" ":
+            data=[0]=1
+        elif name[len(name)-1]==" ":
+            data[1]=1
+
+        if len(password)<8:
+            data[2]=1
+        if not (password==confirm_password):
+            data[3]=1
+
+        if len(email.split(" "))==0 or len(email.split("@"))!=2 or len(email.split("."))<2:
+            data[4]=1
+
+        noerrors=1
+        for error in data:
+            if error==1:
+                noerrors=0
+
+        if noerrors==1:
             key=""
             for val in range(7):
                 key=key+chr(65+random.randint(0,25))
 
             acc.add_account(name, username, password, email, key)
 
-            return render_template("registered.html", mail=email)
-        return render_template("register.html")
+            return render_template("Account_activeren.html", mail=email)
+
+        return render_template("Registreren.html", errors=data)
 
     def login(email, password):
         data=acc.get_account(email, password)
