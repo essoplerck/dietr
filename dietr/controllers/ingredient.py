3c077cac5bcf6b177a47af7af54f19500cb5b0e8
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-22 22:31:24 +0100

    Change route names

--- dietr/controllers/ingredient.py
+++ dietr/controllers/ingredient.py
@@ -7,7 +7,7 @@ model = IngredientModel()
 
 @app.route('/ingredient/<int:id>')
 @app.route('/ingredient/<int:id>/<string:name>')
-def ingredient(id, name = None):
+def view_ingredient(id, name = None):
     '''The view action allows users to view an ingredient.'''
     # Get the ingredient from the database
     ingredient = self.model.get_ingredient(id)
@@ -16,21 +16,21 @@ def ingredient(id, name = None):
     return render_template('ingredient/view.html', ingredient = ingredient)
 
 @app.route('/ingredient/add')
-def ingredient_add():
+def add_ingredient():
     '''The add action allows users to add an ingredient to the database.'''
 
     pass
 
 @app.route('/ingredient/<int:id>/edit')
 @app.route('/ingredient/<int:id>/<string:name>/edit')
-def ingredient_edit(id, name = None):
+def edit_ingredient(id, name = None):
     '''The edit action allows users to change an ingredient.'''
 
     pass
 
 @app.route('/ingredient/<int:id>/delete')
 @app.route('/ingredient/<int:id>/<string:name>/delete')
-def ingredient_delete(id, name = None):
+def delete_ingredient(id, name = None):
     '''The delete action allows users to remove an ingredient for the
     database.
     '''
