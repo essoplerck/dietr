dc1666633e5e7d599bcc1890ff42c045aef65e4b
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-27 18:17:22 +0100

    Change database driver

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
 DEBUG      = False
 SECRET_KEY = 0xff3265ca2324b65066cc5641cce68862a55155cfcaede433
-
-MYSQL_USER     = 'renswnc266_dietr'
-MYSQL_PASSWORD = 'qvuemzxu'
-MYSQL_DB       = 'renswnc266_dietr'
-MYSQL_HOST     = '185.182.57.56'
--- dietr/__init__.py
+++ dietr/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+
+import MySQLdb as MySQL
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-database = MySQL(app)
-database.init_app(app)
+database = MySQL.connect(host   = '185.182.57.56',
+                         user   = 'renswnc266_dietr',
+                         passwd = 'qvuemzxu',
+                         db     = 'renswnc266_dietr')
 
 from .controllers import ingredient, session
