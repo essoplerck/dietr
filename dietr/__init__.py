8b26da9cac39c317846b13f0dd67bcba1a30ec32
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-27 22:38:21 +0100

    Use PyMySQL database driver

--- dietr/__init__.py
+++ dietr/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 
-import MySQLdb as MySQL
+import pymysql as database
+
+connection = database.connect(host        = '185.182.57.56',
+                              user        = 'renswnc266_dietr',
+                              password    = 'qvuemzxu',
+                              db          = 'renswnc266_dietr',
+                              cursorclass = database.cursors.DictCursor)
 
 app = Flask(__name__)
 app.config.from_object('config')
 
-database = MySQL.connect(host   = '185.182.57.56',
-                         user   = 'renswnc266_dietr',
-                         passwd = 'qvuemzxu',
-                         db     = 'renswnc266_dietr')
 
 from .controllers import ingredient, session
--- dietr/models/ingredient.py
+++ dietr/models/ingredient.py
@@ -1,103 +1,67 @@
-from .. import database
+from .. import connection
 
 class IngredientModel:
     '''Model for the ingredient pages. This model will handle all ineractions
     with the database.
     '''
     def add_ingredient(self, ingredient):
-        # Get a cursor
-        cursor = database.cursor()
-
-        # Prepare query
-        cursor.execute('''INSERT INTO ingredients (description, name)
-                               VALUES (%s, %s)''')
+        query = '''INSERT INTO ingredients (description, name)
+                        VALUES (%s, %s)'''
 
         pass
 
     def edit_ingredient(self, ingredient):
-        # Get a cursor
-        cursor = database.cursor()
-
-        # Prepare query
-        cursor.execute('''UPDATE ingredients
-                             SET description = %s,
-                                 name        = %s
-                           WHERE id          = %s''')
+        query = '''UPDATE ingredients
+                      SET description = %s,
+                          name        = %s
+                    WHERE id          = %s'''
 
         pass
 
     def delete_ingredient(self, id):
-        # Get a cursor
-        cursor = database.cursor(MySQL.cursors.DictCursor)
-
-        # Prepare query
-        cursor.execute('''DELETE FROM allergen
-                                WHERE ingredient_id = %s
-                          DELETE FROM ingredient
-                                WHERE id = %s
-                          DELETE FROM recipes_ingredient
-                                WHERE ingredient_id = %s'''
+        query = '''DELETE FROM allergen
+                         WHERE ingredient_id = %s
+                   DELETE FROM ingredient
+                         WHERE id = %s
+                   DELETE FROM recipes_ingredient
+                         WHERE ingredient_id = %s'''
 
         pass
 
     def get_ingredient(self, id):
-        # Get a cursor
-        cursor = database.cursor()
+        query = '''SELECT *
+                     FROM ingredient
+                    WHERE id = %s'''
 
-        # Prepare query
-        cursor.execute('''SELECT *
-                            FROM ingredient
-                           WHERE id = %s''', (id,))
+        cursor = connection.cursor()
+        cursor.execute(query, (id,))
 
-        result = cursor.fetchone()
-
-        ingredient = {
-            'id': result[0],
-            'name': result[1]
-        }
+        ingredient = cursor.fetchone()
 
         return ingredient
 
     def get_allergens(self, id):
-        # Get a cursor
-        cursor = database.cursor()
+        query = '''SELECT category.id, category.name
+                     FROM category
+                          INNER JOIN category_ingredient_relation
+                                  ON category.id = category_ingredient_relation.id
+                   WHERE ingredient_id = %s'''
 
-        # Prepare query
-        cursor.execute('''SELECT category.id, category.name
-                            FROM category
-                                 INNER JOIN category_ingredient_relation
-                                         ON category.id = category_ingredient_relation.id
-                           WHERE ingredient_id = %s''', (id,))
+        cursor = connection.cursor()
+        cursor.execute(query, (id,))
 
-        results = cursor.fetchall()
-
-        allergens = []
-
-        for result in results:
-            allergens.append({
-                'id': result[0],
-                'name': result[1]
-            })
+        allergens = cursor.fetchall()
 
         return allergens
 
     def get_ingredients(self):
-        ingredients = []
+        query = '''SELECT *
+                     FROM ingredient
+                 ORDER BY name'''
 
-        # Get a cursor
-        cursor = database.cursor()
+        cursor = connection.cursor()
+        cursor.execute(query)
 
-        # Prepare query
-        cursor.execute('''SELECT *
-                            FROM ingredient
-                        ORDER BY name''')
-
-        results = cursor.fetchall()
-
-        for result in results:
-            ingredients.append({
-                'id': result[0],
-                'name': result[1]
-            })
+        ingredients = cursor.fetchall()
 
         return ingredients
