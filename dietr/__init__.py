91b0742c60a71a1720515a42d6327ea823c91ebe
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-27 23:12:39 +0100

    Rename imports

--- dietr/__init__.py
+++ dietr/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-import pymysql as database
+import pymysql as sql
 
-connection = database.connect(host        = '185.182.57.56',
-                              user        = 'renswnc266_dietr',
-                              password    = 'qvuemzxu',
-                              db          = 'renswnc266_dietr',
-                              cursorclass = database.cursors.DictCursor)
+connection = sql.connect(host        = '185.182.57.56',
+                         user        = 'renswnc266_dietr',
+                         password    = 'qvuemzxu',
+                         db          = 'renswnc266_dietr',
+                         cursorclass = sql.cursors.DictCursor)
 
 app = Flask(__name__)
 app.config.from_object('config')
