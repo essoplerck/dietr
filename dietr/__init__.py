1a5989267fc408907835fb6bec5a088177f03b16
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-27 15:37:42 +0100

    Prevent global imports

--- dietr/__init__.py
+++ dietr/__init__.py
@@ -3,5 +3,4 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-from .controllers.ingredient import *
-from .controllers.session import *
+from .controllers import ingredient, session
