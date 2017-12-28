7e3cf8141abf25cabe62522cd88faa72ec620ede
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2017-12-28 17:15:49 +0100

    Remove unnecessary files

--- dietr/__init__.py
+++ dietr/__init__.py
@@ -1,7 +1,4 @@
-from functools import wraps
-
-from flask import Flask, session, redirect, url_for
-from flask_session import Session
+from flask import Flask
 
 import pymysql as sql
 
@@ -13,43 +10,3 @@ connection = sql.connect(host        = '185.182.57.56',
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-Session(app)
-
-'''
-@app.teardown_appcontext
-def teardown(exception):
-    # Close database
-    connection.close()
-'''
-
-def login_required(action):
-    '''Decorator for the login required method. This decorator will check if the
-    user is logged in. If not it will redirect to the login page.
-    '''
-    @wraps(action)
-    def login_decorator(*arg, **kwargs):
-        print(session.get('user_id'))
-
-        # Check if user is logged in
-        if 'user_id' in session:
-            return action(*arg, **kwargs)
-        return redirect(url_for('login'))
-    return login_decorator
-
-#from .controllers import ingredient, session
-
-@app.route('/')
-@login_required
-def index():
-    return 'Hello, world!'
-
-@app.route('/login')
-def login():
-    return 'Login'
-
-@app.route('/set')
-def set():
-    session['user_id'] = 1
-
-    return session.get('user_id')
