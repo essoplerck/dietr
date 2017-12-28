46a05bbc69f55d76ba72e3d90641d0bffc7a72d0
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2017-12-28 10:41:13 +0100

    Move view action

--- dietr/controllers/ingredient.py
+++ dietr/controllers/ingredient.py
@@ -5,20 +5,6 @@ from ..models.ingredient import IngredientModel
 
 model = IngredientModel()
 
-@app.route('/ingredient/<int:id>')
-@app.route('/ingredient/<int:id>/<string:name>')
-def view_ingredient(id, name = None):
-    '''The view action allows users to view an ingredient.'''
-    # Get the ingredient from the database
-    ingredient = model.get_ingredient(id)
-
-    # Get the allergens of that ingredient
-    allergens = model.get_allergens(id)
-
-    # Return the template
-    return render_template('ingredient/view.html', ingredient = ingredient,
-                                                   allergens  = allergens)
-
 @app.route('/ingredient/add')
 def add_ingredient():
     '''The add action allows users to add an ingredient to the database.'''
@@ -40,3 +26,18 @@ def delete_ingredient(id, name = None):
     '''
 
     pass
+
+@app.route('/ingredient/<int:id>')
+@app.route('/ingredient/<int:id>/<string:name>')
+def view_ingredient(id, name = None):
+    '''The view action allows users to view an ingredient.'''
+
+    # Get the ingredient from the database
+    ingredient = model.get_ingredient(id)
+
+    # Get the allergens of that ingredient
+    allergens = model.get_allergens(id)
+
+    # Return the template
+    return render_template('ingredient/view.html', ingredient = ingredient,
+                                                   allergens  = allergens)
