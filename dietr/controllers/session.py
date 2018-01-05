73c300a34d6b9e63f4f780f9f9efe0953a7302f0
rensieeee <31250247+rensieeee@users.noreply.github.com>
2018-01-05 14:11:44 +0100

    Sessions
    Added sessions, logging in, logging out, registering and hashing.

--- dietr/controllers/session.py
+++ dietr/controllers/session.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, session
 
 from .. import app, login_required
 from ..models.session import SessionModel
@@ -16,40 +16,26 @@ def login():
         # Get the user from the database
         user = model.get_user(username)
 
-        salt = user['salt']
-        hash = model.get_hash(password, salt)
-
         # Check if the passwords (hashes) match
-        if hash == user['hash']:
+        if pbkdf2_sha256.verify(password, user['hash']):
             # Add user session key
-            pass
-
+            session['id']=user['id']
+            #Return de template van de userpage
         else:
-            error['password'] = 'Password or username is incorect'
-
-            pass
-
-        # Return template
-        pass
-
+            errors={'login': 'Password or username is incorect'}
+            #Return de template van de inlogpage (als die er is)
     else:
-        # Return template
-        pass
+        #Return de template van de inlogpage (als die er is)
 
-@app.route('/logout', methods = ['GET', 'POST'])
+@app.route('/logout', methods = ['POST'])
 @login_required
 def logout():
     '''The logout action allows user to logout.'''
 
-    if request.method == 'POST':
-        # Remove user sessin key
-
-        # Redirect user
-        pass
-
-    else:
-        # Return template
-        pass
+    # Remove user sessin key
+    session.pop('id', None)
+    # Redirect user
+    #Return de template van de uitlogpage (als die er is), of de homepage
 
 @app.route('/join', methods = ['GET', 'POST'])
 def join():
@@ -60,22 +46,56 @@ def join():
     if request.method == 'POST':
         # Get form data
         user = {}
+        user['name']=request.form('name')
+        user['username']=request.form('username')
+        user['password']=request.form('password')
+        user['confirm_password']=request.form('confirm_password')
+        user['email']=request.form('email')
 
         count = model.does_user_exist(user['email'], user['username'])
 
         if count['email']:
-            error['email'] = 'There already is a user with this email address.'
+            errors['email'] = 'There already is a user with this email address.'
 
         if count['username']:
-            error['username'] = 'This user name is already in use'
+            errors['username'] = 'This user name is already in use.'
 
-        if error['email'] or error['username']:
-            # Redirect
-            pass
+        if len(user['name'].split(' '))==0:
+            errors['name']='You have not entered a name.'
+        elif user['name'][0]==' ':
+            errors['name']='You have not entered a first name.'
+        elif user['name'][len(user['name'])-1]==' ':
+            errors['name']='You have not entered a last name.'
 
-        # Redirect
-        pass
+        if len(user['password'])<8 or len(user['password'])>20:
+            errors['password']='Your password is not the required length.'
+        else:
+            lets=0
+            nums=0
+            chars=0
+            for char in user['password']:
+                if char.isdigit():
+                    nums+=1
+                elif char.isalpha():
+                    lets+=1
+                else:
+                    chars+=1
+            if lets==0 or nums==0 or chars==0:
+                errors['password']='Your password does not contain a letter, number and special character.'
+
+        if not (user['password']==user['confirm_password']):
+            errors['confirm_password']='Your entered passwords are not the same.'
+
+        if len(user['email'].split(' '))==0 or len(user['email'].split('@'))!=2 or len(user['email'].split('.'))<2:
+            errors['email']='You have not entered a valid E-mailadress.'
+
+        if len(errors)==0:
+            hash=[pbkdf2_sha256.hash(user['password'])]
+            model.add_user(user['username'], user['name'], hash, user['email'])
+            #Return naar de geregistreerdpagina
+        else:
+            #Return naar de registreerpagina
 
     else:
-        # Return template
+        # Return naar de registreerpagina
         pass
