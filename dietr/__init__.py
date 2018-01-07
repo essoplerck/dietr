5a8126a3ed4793e24fe7f3aa982885a67d18bdea
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-07 12:58:19 +0100

    Move SQL connection to different file

--- dietr/__init__.py
+++ dietr/__init__.py
@@ -1,22 +1,15 @@
-import pymysql as sql
-
 from functools import wraps
+
 from flask import Flask, session, redirect, url_for
 
-connection = sql.connect(host        = '185.182.57.56',
-                         user        = 'renswnc266_dietr',
-                         password    = 'qvuemzxu',
-                         db          = 'renswnc266_dietr',
-                         cursorclass = sql.cursors.DictCursor)
+from .connection import connection
+from .sessions import RedisSessionInterface
 
 app = Flask(__name__,
             template_folder = 'templates',
             static_folder   = 'static')
 
 app.config.from_object('config')
-
-from .sessions import RedisSessionInterface
-
 app.session_interface = RedisSessionInterface()
 
 '''
---
+++ dietr/connection.py
@@ -0,0 +1,7 @@
+import pymysql as sql
+
+connection = sql.connect(host        = '185.182.57.56',
+                         user        = 'renswnc266_dietr',
+                         password    = 'qvuemzxu',
+                         db          = 'renswnc266_dietr',
+                         cursorclass = sql.cursors.DictCursor)
