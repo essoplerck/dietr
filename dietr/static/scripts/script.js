a94d399bd675e58a3cb0af858127a22354bfd266
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-08 21:08:14 +0100

    Create request function

--- dietr/static/scripts/script.js
+++ dietr/static/scripts/script.js
@@ -1,60 +1,42 @@
 let script = window.script || {};
 
+script.request = (url, method = 'GET', body = '') => {
+  let headers = new Headers();
+
+  return fetch(url, {
+    method: method
+  }).then(response => {
+    let status = response.status,
+        type   = response.headers.get("content-type");
+
+    if (type && type.includes('application/json')) {
+      return response.json();
+    }
+
+    throw new TypeError('Response is not JSON');
+  })
+  .catch(error => {});
+}
+
 class Ingredient {
   get(id) {
-    fetch(`/api/ingredients/${id}`, {
-      method: 'GET'
-    }).then(response => {
-      let type = response.headers.get("content-type");
-
-      if (type && type.includes('application/json')) {
-        return response.json();
-      }
-
-      throw new TypeError('Response is not JSON');
-    })
-    .then(response => {
+    script.request(`/api/ingredients/${id}`).then(response => {
       console.log(response)
-    })
-    .catch(error => {});
+    });
   }
 
   // Get a overview of all ingredients
   overview() {
-    fetch(`/api/ingredients`, {
-      method: 'GET'
-    }).then(response => {
-      let type = response.headers.get("content-type");
-
-      if (type && type.includes('application/json')) {
-        return response.json();
-      }
-
-      throw new TypeError('Response is not JSON');
-    })
-    .then(response => {
+    script.request(`/api/ingredients`).then(response => {
       console.log(response)
-    })
-    .catch(error => {});
+    });
   }
 
   // Search the database for ingredients
   search(query) {
-    fetch(`/api/ingredients/search/${query}`, {
-      method: 'GET'
-    }).then(response => {
-      let type = response.headers.get("content-type");
-
-      if (type && type.includes('application/json')) {
-        return response.json();
-      }
-
-      throw new TypeError('Response is not JSON');
-    })
-    .then(response => {
+    script.request(`/api/ingredients/search/${query}`).then(response => {
       console.log(response)
-    })
-    .catch(error => {});
+    });
   }
 }
 
