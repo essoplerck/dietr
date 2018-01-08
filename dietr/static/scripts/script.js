8a33d96ced331ca2ca377b1907779966a8193cdf
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-08 20:31:42 +0100

    Add basic AJAX interface

---
+++ dietr/static/scripts/script.js
@@ -0,0 +1,60 @@
+let script = window.script || {};
+
+script.ingredient = {
+  // Get a ingredient
+  get: id => {
+    fetch(`/api/ingredients/${id}`, {
+      method: 'GET'
+    }).then(response => {
+      let type = response.headers.get("content-type");
+
+      if (type && type.includes('application/json')) {
+        return response.json();
+      }
+
+      throw new TypeError('Response is not JSON');
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {});
+  },
+
+  // Get a overview of all ingredients
+  overview: () => {
+    fetch(`/api/ingredients`, {
+      method: 'GET'
+    }).then(response => {
+      let type = response.headers.get("content-type");
+
+      if (type && type.includes('application/json')) {
+        return response.json();
+      }
+
+      throw new TypeError('Response is not JSON');
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {});
+  },
+
+  // Search the database for ingredients
+  search: query => {
+    fetch(`/api/ingredients/search/${query}`, {
+      method: 'GET'
+    }).then(response => {
+      let type = response.headers.get("content-type");
+
+      if (type && type.includes('application/json')) {
+        return response.json();
+      }
+
+      throw new TypeError('Response is not JSON');
+    })
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {});
+  }
+};
