678f32c5b7eb8c6fb3c51292cdc224bb11c3ba4e
rensieeee <31250247+rensieeee@users.noreply.github.com>
2018-01-08 16:05:03 +0100

    Userpage
    Added Backend for userpage

--- dietr/views/authentication.py
+++ dietr/views/authentication.py
@@ -164,3 +164,53 @@ def join():
 
     # Return template
     return render_template('/authentication/join.html', error=error)
+
+@blueprint.route('/user', methods=['GET', 'POST'])
+def user():
+    if request.method=='POST':
+        new_first_name=request.form['first_name']
+        new_middle_name=request.form['middle_name']
+        new_last_name=request.form['last_name']
+        new_username=request.form['username']
+        current_password=request.form['current_password']
+        new_password=request.form['new_password']
+        new_password_confirm=request.form['new_password_confirm']
+        new_email=request.form['email']
+
+        userdata=model.get_user(sessions['user_id'])
+
+        errors={}
+
+        name=new_firstname
+        if new_firstname and new_last_name:
+            name+=new_middle_name+' '+new_last_name
+        else:
+            errors['name']='You have not entered a valid name.'
+
+        if not new_email:
+            errors['email']='You have not entered a valid E-mailaddress.'
+
+        if not new_username:
+            errors['username']='You have not entered a valid username.'
+
+        if model.verify_hash(current_password, userdata['hash']):
+            new_user={'name': name,
+                      'username': new_username,
+                      'hash': model.generate_hash(new_password),
+                      'email': new_email,
+                      'id': userdata['id']}
+            if new_password:
+                if new_password_confirm==new_password:
+                    model.update_user_with_password(new_user)
+                else:
+                    errors['password']='New passwords do not match.'
+            else:
+                model.update_user_without_password(new_user)
+        else:
+            errors['password']='Invalid password.'
+
+        return render_template(url_for('userpage'), error=errors, userdata=model.get_user(userdata['id']))
+
+    else:
+        userdata=model.get_user(sessions['user_id'])
+        return render_template(url_for('userpage'), user=userdata)
