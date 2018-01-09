6bf0b86d94785f368e5848ef076a3a2b02d67cf6
Daan Renken <daan.renken@gmail.com>
2018-01-09 10:44:42 +0100

    update pictures

---
+++ crawler/pictures.py
@@ -0,0 +1,48 @@
+import os
+import pymysql
+from sys import exit
+
+conn = pymysql.connect(host="185.182.57.56", user="renswnc266_test", passwd="qvuemzxu", db="renswnc266_test", use_unicode=True, charset="utf8")
+myCursor = conn.cursor()
+
+link = os.listdir("C:\\Users\\Daan Renken\\Pictures\\Images")  # locatie foto's
+namen = 'Aardappel-appelhapje met kropsla, ei en ham'  # recept waar een foto bij gezocht moet worden
+
+
+def database(namen):  # zoeken naar id van het recept
+
+    myCursor.execute("SELECT * FROM recipe WHERE name=%s", namen)
+    id = myCursor.fetchall()
+    for t in id:
+        line = ' '.join(str(x) for x in t)
+        recipetid = line.split(' ')[0]
+        relatietabel(recipetid)
+
+
+def relatietabel(recipeid):  # bij het recept de bijbehorende ingredienten id's zoeken
+    myCursor.execute("SELECT * FROM recipe_ingredient_relation WHERE recipe_id=%s", recipeid)
+    id = myCursor.fetchall()
+    for t in id:
+        line = ' '.join(str(x) for x in t)
+        ingredientid = line.split(' ')[2]
+        #  print('ingredientid ' + ingredientid)
+        picture_title(ingredientid)
+
+
+def picture_title(ingredientid):
+    for picture in link:  # opsplitsen en parsen van de namen van de foto's
+        title = picture[:-4]
+        lowercase_title = str(title.lower())
+        myCursor.execute("SELECT * FROM ingredient WHERE name LIKE %s", ("%" + lowercase_title + "%",))  # zoeken van id's die lijken op de foto's
+        id = myCursor.fetchall()
+        for t in id:
+            line = ' '.join(str(x) for x in t)
+            fotoid = line.split(' ')[0]
+            if ingredientid == fotoid:  # de ingredientenid's en fotoíd's vergelijken
+                print(lowercase_title)
+                print('foto ' + picture)
+                exit(0)
+                return picture
+
+
+database(namen)
--- crawler/test.py
+++ crawler/test.py
@@ -1,27 +1,15 @@
-import pymysql
+import os
 
-conn = pymysql.connect(host="185.182.57.56", user="renswnc266_test", passwd="qvuemzxu", db="renswnc266_test", use_unicode=True, charset="utf8")
-myCursor = conn.cursor()
-f = open('file2.txt','w')
-
-def ingredient(idrecept):
-    i = 1
-    while i <= idrecept:
-        myCursor.execute("SELECT * FROM category WHERE id=%s", i)
-        id = myCursor.fetchall()
-        for t in id:
-            line = ' '.join(str(x) for x in t)
-            naam = line.split(' ')[1]
-            print(naam)
-            try:
-                f.write(naam+"\n")
-            except:
-                print('deze lukte helaas niet')
-        i=i+1
-    f.close()
+link = os.listdir("C:\\Users\\Daan Renken\\Pictures\\Images")
 
 
+def picture_title():
+
+    titles = [x[:-4] for x in link]
+    lowercase_title = [x.lower() for x in titles]
+    lowercase_title
+    for t in lowercase_title:
+        print(t)
 
 
-
-ingredient(800)
+picture_title()
