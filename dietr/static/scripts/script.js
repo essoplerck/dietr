9edce8113c5160660af6c3ac7356101c188ddf4a
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-09 11:58:02 +0100

    Add person class

--- dietr/static/scripts/script.js
+++ dietr/static/scripts/script.js
@@ -1,6 +1,6 @@
 let script = window.script || {};
 
-script.request = (url, method = 'GET', body = '') => {
+script.request = (url, method = 'GET') => {
   let headers = new Headers();
 
   return fetch(url, {
@@ -21,23 +21,66 @@ script.request = (url, method = 'GET', body = '') => {
 class Ingredient {
   get(id) {
     script.request(`/api/ingredients/${id}`).then(response => {
-      console.log(response)
+      console.log(response);
     });
   }
 
   // Get a overview of all ingredients
   overview() {
     script.request(`/api/ingredients`).then(response => {
-      console.log(response)
+      console.log(response);
     });
   }
 
   // Search the database for ingredients
   search(query) {
     script.request(`/api/ingredients/search/${query}`).then(response => {
-      console.log(response)
+      console.log(response);
     });
   }
 }
 
 script.ingredient = new Ingredient();
+
+class Person {
+  constructor() {
+    this.allergies = {
+      add: id => {
+        if (!this.handle) {
+          return
+        }
+
+        url = `/api/persons/${this.handle}/allergies/${id}`;
+
+        script.request(url, 'POST').then(response => {
+          console.log(response);
+        });
+      }
+    }
+
+    this.ingredient = {
+      add: id => {
+        if (!this.handle) {
+          return
+        }
+
+        url = `/api/persons/${this.handle}/ingredients/${id}`
+
+        script.request(url, 'POST').then(response => {
+          console.log(response);
+        });
+      }
+    }
+  }
+
+  get handle() {
+    let location = window.location.pathname,
+        path     = location.split('/');
+
+    if (path[1] === 'person' && path[2]) {
+      return path[2];
+    }
+  }
+}
+
+script.person = new Person();
