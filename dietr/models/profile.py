34d57d0c91336f4bf99211ee8b59ee750c7dd356
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2018-01-10 11:17:21 +0100

    Inherit authentication model

--- dietr/models/profile.py
+++ dietr/models/profile.py
@@ -1,29 +1,12 @@
 from passlib.hash import pbkdf2_sha256
 
 from .. import app, connection
+from . import AuthenticationModel
 
-
-class ProfileModel:
+class ProfileModel(AuthenticationModel):
     '''Model for the profile page. This model will handle all
     ineractions with the database and cryptography.
     '''
-    def generate_hash(self, password, salt=None):
-        return pbkdf2_sha256.hash(password)
-
-    def verify_hash(self, password, hash, salt=None):
-        return pbkdf2_sha256.verify(password, hash)
-
-    def get_user(self, id):
-        query = '''SELECT *
-                     FROM account
-                    WHERE id = %s'''
-
-        cursor = connection.cursor()
-        cursor.execute(query, id)
-
-        # Return user
-        return cursor.fetchone()
-
     def update_user_with_password(self, user):
         query = '''UPDATE account
                       SET name = %s, username = %s, hash = %s, email = %s
@@ -47,20 +30,3 @@ class ProfileModel:
 
         # Execute query
         return connection.commit()
-
-    def does_user_exist(self, email, username):
-        query = '''SELECT (
-                          SELECT COUNT(email)
-                            FROM account
-                           WHERE email = %s
-                          ) AS email, (
-                          SELECT COUNT(username)
-                            FROM account
-                           WHERE username = %s
-                          ) AS username'''
-
-        cursor = connection.cursor()
-        cursor.execute(query, (email, username))
-
-        # Retrun number of matching mail adresses and usernames
-        return cursor.fetchone()
