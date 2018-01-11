7f4e0147eb076b3a4baf689b34e60997b1108b22
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-11 11:19:13 +0100

    Add helper functions

--- dietr/connection.py
+++ dietr/connection.py
@@ -3,3 +3,24 @@ import pymysql as sql
 connection = sql.connect(host='185.182.57.56', user='renswnc266_dietr',
                          password='qvuemzxu', db='renswnc266_dietr',
                          cursorclass=sql.cursors.DictCursor)
+
+
+def commit(query, arugments=()):
+    with connection.cursor() as cursor:
+        cursor.execute(query, arugments)
+
+    connection.commit()
+
+
+def fetch(query, arugments=()):
+    with connection.cursor() as cursor:
+        cursor.execute(query, arugments)
+
+    return cursor.fetchone()
+
+
+def fetch_all(query, arugments=()):
+    with connection.cursor() as cursor:
+        cursor.execute(query, arugments)
+
+    return cursor.fetchall()
