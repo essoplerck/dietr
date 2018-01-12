8abcf5036fc7eb6e7882c60a4317a716596af230
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2018-01-12 13:29:15 +0100

    Update password

--- dietr/connection.py
+++ dietr/connection.py
@@ -1,16 +1,16 @@
 import pymysql as sql
 
 connection = sql.connect(host='185.182.57.56', user='renswnc266_dietr',
-                         password='qvuemzxu', db='renswnc266_dietr',
+                         password='69f5854300d331e7', db='renswnc266_dietr',
                          cursorclass=sql.cursors.DictCursor)
 
 
 class Database:
     def __init__(self):
-        self.connection = sql.connect(db='renswnc266_dietr',
+        self.connection = sql.connect(db='renswnc266_development',
                                       host='185.182.57.56',
-                                      password='qvuemzxu',
-                                      user='renswnc266_dietr')
+                                      user='renswnc266_dietr',
+                                      password='69f5854300d331e7')
 
     def commit(self, query, arugments=()):
         with self.connection.cursor() as cursor:
