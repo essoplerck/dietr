f7d1c1f59c8dd96309ab74660d180ad7b847846f
Sem van Nieuwenhuzien <essoplerck@gmail.com>
2018-01-12 21:07:34 +0100

    Use dataclasses

--- dietr/api/resources/person.py
+++ dietr/api/resources/person.py
@@ -9,42 +9,34 @@ prefix = '/persons/<int:handle>'
 
 class PersonsAllergies(Resource):
     def delete(self, handle, id):
-        user = 1
 
-        query = '''DELETE pc
-                     FROM person_category_relation AS pc
-                          INNER JOIN category
-                             ON category.id = pc.category_id
-                          INNER JOIN person
-                             ON person.id   = pc.person_id
-                    WHERE category.id       = %s
-                      AND person.account_id = %s
-                      AND person.handle     = %s'''
+        user_id = session['user']
 
-        cursor = connection.cursor()
-        status = cursor.execute(query, (id, user, handle))
+        query = '''DELETE roommates_allergies
+                     FROM roommates_allergies
+                          INNER JOIN allergies
+                             ON allergens.id = roommates_allergies.allergy_id
+                          INNER JOIN roommates
+                             ON roommates.id = roommates_allergies.roommate_id
+                    WHERE allergies.id = %s
+                      AND roommates.handle = %s
+                      AND roommates.user_id = %s'''
 
-        connection.commit()
-
-        return status
+        database.commit(query, (id, handle, user_id))
 
     def post(self, handle, id, flag=0):
-        user = 1
 
-        query = '''INSERT INTO person_category_relation (person_id, category_id)
-                   SELECT person.id, %s
-                     FROM person_category_relation AS pc
-                          INNER JOIN person
-                             ON person.id   = pc.person_id
-                    WHERE person.account_id = %s
-                      AND person.handle     = %s'''
+        user_id = session['user']
 
-        cursor = connection.cursor()
-        status = cursor.execute(query, (id, user, handle))
+        query = '''INSERT INTO ra (roommate_id, allergy_id, flag)
+                   SELECT roommates.id, %s, %s
+                     FROM roommates_allergies AS ra
+                          INNER JOIN roommates
+                             ON roommates.id = ra.roommates_id
+                    WHERE roommates.handle = %s
+                      AND roommates.user_id = %s'''
 
-        connection.commit()
-
-        return status
+        database.commit(query, (id, flag, handle, user_id))
 
 
 api.add_resource(PersonsAllergies, f'{prefix}/allergies/<int:id>')
@@ -52,43 +44,34 @@ api.add_resource(PersonsAllergies, f'{prefix}/allergies/<int:id>')
 
 class PersonsIngredients(Resource):
     def delete(self, handle, id):
-        user = 1
 
-        query = '''DELETE pi
-                     FROM person_ingredient_relation AS pi
-                          INNER JOIN ingredient
-                             ON ingredient.id = pi.ingredient_id
-                          INNER JOIN person
-                             ON person.id     = pi.person_id
-                    WHERE ingredient.id       = %s
-                      AND person.account_id   = %s
-                      AND person.handle       = %s'''
+        user_id = session['user']
 
-        cursor = connection.cursor()
-        status = cursor.execute(query, (id, user, handle))
+        query = '''DELETE rp
+                     FROM roommates_preferences AS rp
+                          INNER JOIN ingredients
+                             ON preferences.id = rp.ingredient_id
+                          INNER JOIN roommates
+                             ON roommates.id = rp.roommate_id
+                    WHERE ingredients.id = %s
+                      AND roommates.handle = %s
+                      AND roommates.user_id = %s'''
 
-        connection.commit()
+        database.commit(query, (id, handle, user_id))
 
-        return status
-
-    # @FIXME prevent from adding 4 enties
     def post(self, handle, id, flag=0):
-        user = 1
 
-        query = '''INSERT INTO person_ingredient_relation (person_id, ingredient_id)
-                     SELECT person.id, %s
-                       FROM person_category_relation AS pc
-                            INNER JOIN person
-                               ON person.id   = pc.person_id
-                      WHERE person.account_id = %s
-                        AND person.handle     = %s'''
+        user_id = session['user']
 
-        cursor = connection.cursor()
-        status = cursor.execute(query, (id, user, handle))
+        query = '''INSERT INTO rp (roommate_id, ingredient_id, flag)
+                   SELECT roommates.id, %s, %s
+                     FROM roommates_preferences AS rp
+                          INNER JOIN roommates
+                             ON roommates.id = rp.roommates_id
+                    WHERE roommates.handle = %s
+                      AND roommates.user_id = %s'''
 
-        connection.commit()
-
-        return status
+        database.commit(query, (id, flag, handle, user_id))
 
 
 api.add_resource(PersonsIngredients, f'{prefix}/ingredients/<int:id>')
