c733f9d5cb0ee7ee98a829cd93351f79b5d33860
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-14 19:34:07 +0100

    Add allergy model

--- dietr/models/allergy.py
+++ dietr/models/allergy.py
@@ -8,4 +8,40 @@ class Allergy:
 
 
 class AllergyModel:
-    pass
+    def add_allergy(self, name):
+        query = '''INSERT INTO allergies (name)
+                   VALUES (%s)'''
+
+        # Execute query
+        database.commit(query, name)
+
+    def delete_allergy(self, id):
+        query = '''DELETE FROM allergies
+                    WHERE id = %s'''
+
+        # Execute query
+        database.commit(query, id)
+
+    def get_allergy(self, id):
+        query = '''SELECT id, name
+                     FROM allergies
+                    WHERE id = %s'''
+
+        return Allergy(**database.fetch(query, id))
+
+    def get_allergies(self):
+        query = '''SELECT id, name
+                     FROM allergies
+                    ORDER BY name'''
+
+        allergies = database.fetch_all(query)
+
+        return [Allergy(**allergy) for allergy in allergies]
+
+    def set_allergy(self, id, name):
+        query = '''UPDATE allergies
+                      SET name = %s
+                    WHERE id = %s'''
+
+        # Execute query
+        database.commit(query, (name, id))
