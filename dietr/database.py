9c221c17df44b0a2425c575bddd53530c5a5a6c5
Sem van Nieuwenhuizen <essoplerck@gmail.com>
2018-01-17 09:49:34 +0100

    Add documentation

--- dietr/database.py
+++ dietr/database.py
@@ -5,13 +5,19 @@ from dietr.utils import singleton
 
 @singleton
 class Database:
+    """Connects to the MySQL database and provides helper functions. Alls data
+    is return in the form of dictionaries. This class is a singelton, there is
+    only one instance.
+    """
     def __init__(self):
         self.connection = None
 
     def close(self):
+        """Close database connection."""
         self.connection.close()
 
     def connect(self):
+        """Connecto to the database."""
         self.connection = sql.connect(database='renswnc266_development',
                                       host='185.182.57.56',
                                       user='renswnc266_dietr',
@@ -19,22 +25,26 @@ class Database:
                                       cursorclass=sql.cursors.DictCursor)
 
     def commit(self, query, arugments=()):
+        """Execute a query and commit."""
         with self.connection.cursor() as cursor:
             cursor.execute(query, arugments)
 
             self.connection.commit()
 
     def fetch(self, query, arugments=()):
+        """Fetch one row from the database."""
         with self.connection.cursor() as cursor:
             cursor.execute(query, arugments)
 
             return cursor.fetchone()
 
     def fetch_all(self, query, arugments=()):
+        """Fetch many rows from the database."""
         with self.connection.cursor() as cursor:
             cursor.execute(query, arugments)
 
             return cursor.fetchall()
 
 
+# Create an instance of the database class
 database = Database()
